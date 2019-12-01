146134572508067b05afe25196904d5527603b34
Juwan <junhuanchen@qq.com>
2019-12-01 15:19:40 +0800

    TS

--- src/ESP8266AudioMP3.cpp
+++ src/ESP8266AudioMP3.cpp
@@ -57,12 +57,12 @@ extern "C"
 // To run, set your ESP8266 build to 160MHz, update the SSID info, and upload.
 
 // Enter your WiFi setup here:
-// const char *SSID = "zhiwu";
-// const char *PASSWORD = "zhiwu123";
+const char *SSID = "zhiwu";
+const char *PASSWORD = "zhiwu123";
 
 // Enter your WiFi setup here:
-const char *SSID = "weisi";
-const char *PASSWORD = "WEIsi123456";
+// const char *SSID = "weisi";
+// const char *PASSWORD = "WEIsi123456";
 
 AudioGeneratorMP3 *mp3;
 AudioFileSourceHTTPStream *file;
@@ -103,6 +103,8 @@ static uint8_t buffer_mp3[RX_BUF_MAX] = {};
 
 #include "samplemp3.h"
 
+#include "sdcard.hpp"
+
 int http_get_mp3(uint8_t *buffer, AudioFileSourceHTTPStream *file, uint32_t recv_max = RX_BUF_MAX)
 {
   file->SetReconnect(true, 1000);
@@ -135,28 +137,34 @@ void setup()
 {
   system_update_cpu_freq(SYS_CPU_160MHZ);
 
+  pinMode(16 , OUTPUT);
+  digitalWrite(16 , HIGH);
+
   // serialCmd.begin(115200);
   // serialCmd.println("Connecting to Control");
 
   Serial.begin(115200);
   delay(1000);
-  Serial.println("Connecting to WiFi");
 
-  WiFi.disconnect();
-  WiFi.softAPdisconnect(true);
-  WiFi.mode(WIFI_STA);
+  // Serial.println("Connecting to WiFi");
 
-  WiFi.begin(SSID, PASSWORD);
+  // WiFi.disconnect();
+  // WiFi.softAPdisconnect(true);
+  // WiFi.mode(WIFI_STA);
 
-  // Try forever
-  while (WiFi.status() != WL_CONNECTED)
-  {
-    Serial.println("...Connecting to WiFi");
-    delay(1000);
-  }
-  Serial.println("Connected");
+  // WiFi.begin(SSID, PASSWORD);
 
-  judge_init();
+  // // Try forever
+  // while (WiFi.status() != WL_CONNECTED)
+  // {
+  //   Serial.println("...Connecting to WiFi");
+  //   delay(1000);
+  // }
+  // Serial.println("Connected");
+
+  sdcard_test();
+
+  // judge_init();
 
   audioLogger = &Serial;
 
@@ -164,11 +172,45 @@ void setup()
 
   // const char *URL = "http://dict.youdao.com/dictvoice?audio=hello+everyone,ii&type=3";
 
-  const char *URL = "http://dict.youdao.com/dictvoice?audio=hello+everyone,fin&type=3";
+  // const char *URL = "http://dict.youdao.com/dictvoice?audio=hello+everyone,fin&type=3";
+  // file = new AudioFileSourceHTTPStream(URL);
+  // http_get_mp3(buffer_mp3, file);
 
-  file = new AudioFileSourceHTTPStream(URL);
+  const char *mp3_name = "hello+everyone.mp3";
 
-  http_get_mp3(buffer_mp3, file);
+  if (SD.exists(mp3_name)) {
+
+    Serial.println("file exists.");
+    File fe = SD.open(mp3_name);
+
+    uint8_t *tmp = &buffer_mp3[0];
+    uint32_t size = fe.size();
+
+    if(fe.isFile())
+    {
+      Serial.printf("file %d size %d\n", fe.isFile(), fe.size());
+
+      while (fe.available())
+      {
+        // Serial.printf("%02X\n", fe.read());
+        *tmp = fe.read();
+        tmp++;
+      }
+      fe.close();
+    }
+
+    // Serial.printf("buffer_mp3 size %d %d \n", size, tmp - buffer_mp3);
+
+    in = new AudioFileSourcePROGMEM(buffer_mp3, tmp - buffer_mp3);
+    // in = new AudioFileSourcePROGMEM(buffer_mp3, file->getSize());
+    out = new AudioOutputI2S(0, true);
+    mp3 = new AudioGeneratorMP3();
+    mp3->RegisterStatusCB(StatusCallback, (void *)"mp3");
+    mp3->begin(in, out);
+
+  } else {
+    Serial.println("file doesn't exist.");
+  }
 
   // buff = new AudioFileSourceBuffer(file, 8192);
   // buff->RegisterStatusCB(StatusCallback, (void*)"buffer");
@@ -176,16 +218,15 @@ void setup()
 
   // in = new AudioFileSourcePROGMEM(samplemp3, sizeof(samplemp3));
 
-  in = new AudioFileSourcePROGMEM(buffer_mp3, file->getSize());
-  out = new AudioOutputI2S(0, true);
-  mp3 = new AudioGeneratorMP3();
-  mp3->RegisterStatusCB(StatusCallback, (void *)"mp3");
-  mp3->begin(in, out);
 }
 
 void loop()
 {
   static int lastms = 0;
+
+  // Serial.printf("Running for %d ms...\n", lastms);
+
+  // delay(1000);
 
   if (mp3->isRunning())
   {
@@ -194,11 +235,12 @@ void loop()
       mp3->stop();
     }
 
-    // if (millis()-lastms > 1000) {
-    //   lastms = millis();
-    //   Serial.printf("Running for %d ms...\n", lastms);
-    //   Serial.flush();
-    //  }
+    if (millis() - lastms > 1000)
+    {
+      lastms = millis();
+      Serial.printf("Running for %d ms...\n", lastms);
+      Serial.flush();
+    }
 
     if (!mp3->loop())
     {
@@ -209,28 +251,28 @@ void loop()
   {
     delay(1000);
 
-    // if (serialCmd.available())
-    String tmp = judge_check();
-    if (tmp.length() != 0)
-    {
-      Serial.printf("MP3 ready\n");
+    // // if (serialCmd.available())
+    // String tmp = judge_check();
+    // if (tmp.length() != 0)
+    // {
+    //   Serial.printf("MP3 ready\n");
 
-      // String tmp = serialCmd.readString();
-      Serial.println(tmp);
+    //   // String tmp = serialCmd.readString();
+    //   Serial.println(tmp);
 
-      static char URL[0xFF] = {};
+    //   static char URL[0xFF] = {};
 
-      sprintf(URL, "http://dict.youdao.com/dictvoice?audio=%s,fin&type=1", tmp.c_str());
+    //   sprintf(URL, "http://dict.youdao.com/dictvoice?audio=%s,fin&type=1", tmp.c_str());
 
-      Serial.printf(URL);
+    //   Serial.printf(URL);
 
-      // const char *URL = "http://dict.youdao.com/dictvoice?audio=banana,fin&type=3";
+    //   // const char *URL = "http://dict.youdao.com/dictvoice?audio=banana,fin&type=3";
 
-      file->open(URL);
-      http_get_mp3(buffer_mp3, file);
-      in->open(buffer_mp3, file->getSize());
+    //   file->open(URL);
+    //   http_get_mp3(buffer_mp3, file);
+    //   in->open(buffer_mp3, file->getSize());
 
-      mp3->begin(in, out);
-    }
+    //   mp3->begin(in, out);
+    // }
   }
 }
